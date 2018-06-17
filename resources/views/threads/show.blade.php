5957303763e7fb25152c4e5e89394202d43873cc
Viktor <viktor.malmberg@officemanagement.se>
2018-06-17 20:04:04 +0200

    added tests for replies and replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -15,5 +15,20 @@
             </div>
         </div>
     </div>
+       <div class="row justify-content-center">
+        <div class="col-md-8 col-md-offset">
+            @foreach ($thread->replies as $reply)
+             <div class="card-header mt-4">{{$reply->created_at->diffForHumans()}}</div>
+            <div class="card ">
+                <div class="card-body">
+                        <div class="panel-body">
+                            {{$reply->body}}
+                        </div>
+                </div>
+            </div>
+            @endforeach
+
+        </div>
+    </div>
 </div>
 @endsection
---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+Use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertSee($this->thread->body);
+
+
+    }
+    /** @test */
+    public function a_user_can_view_specific_threads()
+    {
+        $this->get('/threads/'. $this->thread->id)
+        ->assertSee($this->thread->title);
+    }
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = factory('App\Reply')
+                ->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/'.$this->thread->id)
+                ->assertSee($reply->body);
+
+    }
+}
+
+
+
