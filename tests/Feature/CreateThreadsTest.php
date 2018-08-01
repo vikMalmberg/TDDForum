27e2203b53c87167542f9c8586f7de8121e77522
Viktor <viktor.malmberg@officemanagement.se>
2018-08-01 06:20:47 +0200

    added helper SIgnIn method

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -22,7 +22,7 @@ class CreateThreadsTest extends TestCase
     /** @test */
     public function an_authenticated_user_can_create_new_forum_threads()
     {
-        $this->actingAs(create('App\User'));
+        $this->signIn();
 
         $thread = factory('App\Thread')->make();
 
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function signIn($user = null)
+    {
+        // if user is null create a user
+        $user = $user ?: create('App\User');
+
+        $this->actingAs($user);
+
+        return $this;
+    }
 }
